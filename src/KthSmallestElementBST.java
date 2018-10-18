98cad4d19cd7935e2523071489fa91afd7717655
Pavlo Kazarin <x.pasha6@gmail.com>
2018-10-18 23:32:55 +0300

    Update KthSmallestElementBST.java

--- src/KthSmallestElementBST.java
+++ src/KthSmallestElementBST.java
@@ -1,1 +1,32 @@
 
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+       //        Stack<TreeNode> stack = new Stack<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode node = root;
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+//        int i = 0;
+        while (!stack.isEmpty()) {
+            TreeNode stackTopElement = stack.pop();
+//            i++;
+            k--;
+            if (0 == k)
+                return stackTopElement.val;
+
+            TreeNode r = stackTopElement.right;
+            while (r != null) {
+                stack.push(r);
+                r = r.left;
+            }
+
+        }
+
+        return -1;
+
+
+
+    }
+}
