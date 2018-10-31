ff1745f0388f2a80e560b40065faa0dfdaab0552
Pavlo Kazarin <x.pasha6@gmail.com>
2018-10-31 19:13:05 +0200

    Update DesignTwitter.java

--- src/DesignTwitter.java
+++ src/DesignTwitter.java
@@ -1,109 +1,116 @@
+package leetcode.medium.design_twitter;
+
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
 
-class Twitter {
+/**
+ * This problem is solved and was approved by leetcode
+ *
+ * @author Pavel Kazarin
+ * @see <a href="https://leetcode.com/problems/design-twitter/description/">Design Twitter</a>
+ * @see leetcode.medium.design_twitter.Tweet
+ */
+class DesignTwitter {
 
-    //stores userID(follower) as key and userID(folowee) as value
-    HashMap<Integer, HashSet<Integer>> followingMap;
+    // Stores userID(follower) as key and userID(folowee) as value
+    private ConcurrentMap<Integer, Set<Integer>> followingMap;
 
-    //stores userID as key and hashmap of priorities(key) and tweetID(value) as value
-    HashMap<Integer, HashMap<Integer, Integer>> tweetMap;
-
-    int priorityCounter;
+    // Stores userID as key and list of {@code Tweet} objects as value
+    private ConcurrentMap<Integer, List<Tweet>> tweetMap;
 
     /**
-     * Initialize your data structure here.
+     * Constructor that initializes data structure
      */
-    public Twitter() {
-        followingMap = new HashMap<>();
-        tweetMap = new HashMap<>();
-        priorityCounter = 0;
+    DesignTwitter() {
+        this.followingMap = new ConcurrentHashMap<>();
+        this.tweetMap = new ConcurrentHashMap<>();
     }
 
     /**
-     * Compose a new tweet.
+     * This method composes a new tweet.
+     *
+     * @param userId  User's id
+     * @param tweetId Tweet's id
      */
-    public void postTweet(int userId, int tweetId) {
-        HashMap<Integer, Integer> usersTweetsMap = tweetMap.get(userId);
-        if (usersTweetsMap == null) {
-            usersTweetsMap = new HashMap<>();
-            tweetMap.put(userId, usersTweetsMap);
-        }
-        usersTweetsMap.put(priorityCounter, tweetId);
-        priorityCounter++;
+    void postTweet(int userId, int tweetId) {
+        List<Tweet> userTweetsList = tweetMap.computeIfAbsent(userId, k -> new CopyOnWriteArrayList<>());
+        userTweetsList.add(new Tweet(tweetId, System.nanoTime()));
     }
 
     /**
-     * Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+     * Retrieve the 10 most recent tweet ids in the user's news feed.
+     * Each item in the news feed is posted by user or who the user followed or by the user herself
+     *
+     * @param userId User's id
+     * @return List of 10 most recent tweet
      */
-    public List<Integer> getNewsFeed(int userId) {
+    List<Integer> getNewsFeed(int userId) {
 
-        //tree to store and sort all tweets we will need
-        TreeMap<Integer, Integer> tweetsToReturnTreeMap = new TreeMap();
+        // List to store all tweets related to this user
+        List<Tweet> listOfTweets = new ArrayList<>();
 
-        //list to return
-        List<Integer> listToReturn = new ArrayList<>();
+        // Getting all tweets by current user
+        List<Tweet> usersTweets = tweetMap.get(userId);
 
-        //getting all tweets by current user
-        HashMap<Integer, Integer> usersTweets = tweetMap.get(userId);
-
-        //adding all user's tweets to Treemap
+        // Adding all user's tweets to Treemap
         if (usersTweets != null) {
-            tweetsToReturnTreeMap.putAll(usersTweets);
+            listOfTweets.addAll(usersTweets);
         }
-        //geting user's folowees
-        HashSet<Integer> usersFolowees = followingMap.get(userId);
+        // Getting user's folowees
+        Set<Integer> usersFolowees = followingMap.get(userId);
 
-        //adding all folowee's tweets to TreeMap
-       if (usersFolowees != null && usersFolowees.size()!=0) {
+        // Adding all folowee's tweets to TreeMap
+        if (usersFolowees != null && usersFolowees.size() != 0) {
             for (int foloweeId : usersFolowees) {
-                if (tweetMap.get(foloweeId) != null) {
-                    tweetsToReturnTreeMap.putAll(tweetMap.get(foloweeId));
+                List<Tweet> tweets = tweetMap.get(foloweeId);
+                if(tweets!=null){
+                    listOfTweets.addAll(tweets);
                 }
             }
         }
 
-        // for (int i = tweetsToReturnTreeMap.keySet().size() - 1; i >= 0; i--) {
-        //     listToReturn.add(tweetsToReturnTreeMap.get(i));
-        //     if (listToReturn.size() >= 10) {
-        //         break;
-        //     }
-        // }
-        NavigableSet<Integer> descKeys = tweetsToReturnTreeMap.descendingKeySet();
+        // Sorting tweets by priority value
+        // New implementation, that has worse runtime
+        listOfTweets.sort(Comparator.comparingLong(Tweet::getTweetPriority).reversed());
 
-        int counter = 0;
-        for(int i : descKeys){
-            if(counter>=10){
-                return listToReturn;
-            }
-            listToReturn.add(tweetsToReturnTreeMap.get(i));
-            counter++;
+        // Old implementation with better runtime
+//        listOfTweets.sort((Tweet o1, Tweet o2) -> Long.compare(o2.getTweetPriority(), o1.getTweetPriority()));
+
+        // Adding tweetsID to listToReturn
+        List<Integer> listToReturn = new ArrayList<>();
+        for (int i = 0; i < 10 && i < listOfTweets.size(); i++) {
+            listToReturn.add(listOfTweets.get(i).getTweetId());
         }
-
-
         return listToReturn;
     }
 
     /**
-     * Follower follows a followee. If the operation is invalid, it should be a no-op.
+     * This method makes follower following followee
+     *
+     * @param followerId Follower's id
+     * @param followeeId Followee's id
      */
-    public void follow(int followerId, int followeeId) {
-        HashSet<Integer> followeeSet = followingMap.get(followerId);
-        if (followeeSet == null) {
-            followeeSet = new HashSet<>();
-            followingMap.put(followerId, followeeSet);
-        }
+    void follow(int followerId, int followeeId) {
+        Set<Integer> followeeSet = followingMap.computeIfAbsent(followerId, k -> new CopyOnWriteArraySet<>());
+        if (followeeId == followerId)
+            return;
         followeeSet.add(followeeId);
     }
 
     /**
-     * Follower unfollows a followee. If the operation is invalid, it should be a no-op.
+     * This method makes follower unfollowing folowee
+     *
+     * @param followerId follower's id
+     * @param followeeId folowee's id
      */
-    public void unfollow(int followerId, int followeeId) {
-        HashSet<Integer> followeeSet = followingMap.get(followerId);
-        if(followeeSet == null){
+    void unfollow(int followerId, int followeeId) {
+        Set<Integer> followeeSet = followingMap.get(followerId);
+        if (followeeSet == null) {
             return;
         }
-        
         followeeSet.remove(followeeId);
     }
 }
